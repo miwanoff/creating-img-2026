f5e63de894c6879f301dc841ef294a6d20405c35
miwanoff <marina.iva.noffa@gmail.com>
2026-05-09 10:54:54 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function createImg() {
+  const parent = document.getElementById("kit");
+  let img = document.createElement("img");
+  img.setAttribute("src", "images/fl1.jpg");
+  parent.appendChild(img);
+}
